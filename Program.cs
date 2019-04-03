0323495904952e78f9a505c5a6a1d3fc0e4691e1
Arzola <contact@heisarzola.com>
2019-04-03 14:37:58 -0700

    Logging Service

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration.Install;
+using System.Reflection;
+using System.ServiceProcess;
+
+namespace Logging_Service
+{
+    static class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // This is meant to debug things while still on the IDE
+#if DEBUG
+            Service service = new Service();
+            service.DebugStart();
+            // Acts as a while true
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+            return;
+#endif
+            // This allows us to give the .exe parameters that allow it to install and uninstall itself without much trouble.
+            // (So long as the command line is run as administrator of course).
+            if (Environment.UserInteractive)
+            {
+                string parameter = string.Concat(args);
+                switch (parameter)
+                {
+                    case "--install":
+                        ManagedInstallerClass.InstallHelper(new[] { Assembly.GetExecutingAssembly().Location });
+                        break;
+                    case "--uninstall":
+                        ManagedInstallerClass.InstallHelper(new[] { "/u", Assembly.GetExecutingAssembly().Location });
+                        break;
+                }
+                return;
+            }
+
+            ServiceBase[] servicesToRun = new ServiceBase[]
+            {
+                new Service()
+            };
+            ServiceBase.Run(servicesToRun);
+        }
+    }
+}
---
+++ Service.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace Logging_Service
+{
+    public partial class Service : ServiceBase
+    {
+        private static System.Timers.Timer _timer;
+        private int _elapsedSeconds = 0;
+
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        // Remnant of tests
+        public void DebugStart()
+        {
+
+        }
+
+        private void PrepareLogging()
+        {
+            EventLog.Log = "Application"; // Set logs to the application category
+            AutoLog = false;
+
+            ((ISupportInitialize)this.EventLog).BeginInit();
+            if (!EventLog.SourceExists(this.ServiceName))
+            {
+                EventLog.CreateEventSource(this.ServiceName, "Application");
+            }
+            ((ISupportInitialize)this.EventLog).EndInit();
+
+            EventLog.Source = this.ServiceName;
+        }
+
+        private void OnEverySecond(object source, ElapsedEventArgs e)
+        {
+            _elapsedSeconds++;
+
+            EventLog.WriteEntry("Example Log");
+
+            if (_elapsedSeconds % 5 == 0)
+            {
+                EventLog.WriteEntry("Example Warning", EventLogEntryType.Warning);
+            }
+
+            if (_elapsedSeconds % 30 == 0)
+            {
+                EventLog.WriteEntry("Example Error", EventLogEntryType.Error);
+            }
+
+            if (_elapsedSeconds > 59) { _elapsedSeconds = 0; }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            PrepareLogging();
+
+            // Create a timer with a one second interval.
+            _timer = new System.Timers.Timer(1000);
+
+            // Hook up the Elapsed event for the timer.
+            _timer.Elapsed += new ElapsedEventHandler(OnEverySecond);
+
+            _timer.Interval = 1000;
+            _timer.Enabled = true;
+            // System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStart.txt");
+        }
+
+        protected override void OnStop() { }
+    }
+}
